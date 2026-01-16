ed00f327b47fc8ef74ba13a25466d8dab3d59a5c
khata <hatakenaka@byui.edu>
2026-01-15 20:57:57 -0700

    Prep 3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Console.Write("What is the magic number? ");
+        string magicAnswer = Console.ReadLine();
+        int magicNumber = int.Parse(magicAnswer);
+        int guessNumber, count = 0;
+        string response = null;
+        
+        do {
+            do 
+            {
+                count += 1;
+                Console.Write("What is your guess? ");
+                string guessAnswer = Console.ReadLine();
+                guessNumber = int.Parse(guessAnswer);
+                
+            
+                if (guessNumber == magicNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took {count} tries.");
+                }
+                else if (guessNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Higher");
+                }
+            } while (guessNumber != magicNumber);
+            
+            Console.Write("Do you still want to play? ");
+            response = Console.ReadLine();
+
+            } while (response != "yes");
     }
 }
