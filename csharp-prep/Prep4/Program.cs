476bab183d38618d2e6b1ece7f53130c5ee9060d
khata <hatakenaka@byui.edu>
2026-01-15 22:25:42 -0700

    Prep 4 done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number = 0, sum = 0, largest = 0;
+        List<int> numberList = new List<int>();
+        do
+        {   
+            
+
+            Console.Write("Enter number: ");
+            string inputNumber = Console.ReadLine();
+            number = int.Parse(inputNumber);
+            if (number != 0)
+            {
+                numberList.Add(number);
+            }
+
+            
+            
+        } while (number != 0);
+
+        foreach (int eachNum in numberList)
+        {
+            sum = sum + eachNum;
+
+            if (eachNum >= largest)
+            {
+                largest = eachNum;
+            }
+        }
+
+       
+        double avg = (double)sum / (double)numberList.Count;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {avg}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
