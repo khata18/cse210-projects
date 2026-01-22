4e75e7570971220d7b5a0c6cddb9f3b45ad01830
khata <hatakenaka@byui.edu>
2026-01-21 22:52:33 -0700

    worked on Prep 5 until PromptUserBirthYear()

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Configuration.Assemblies;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        PromptUsername();
+        PromptUserNumber();
+
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program");
+    }
+    static void PromptUsername()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string favNumber = Console.ReadLine();
+        int number =  int.Parse(favNumber);
+        return number;
+    }
+    static void PromptUserBirthYear(out int birthYear)
+    {
+        
+    }
+    
+
 }
