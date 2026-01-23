6673e59565aaacde4a57ec070450eb1f59141acb
khata <hatakenaka@byui.edu>
2026-01-22 18:49:29 -0700

    Completed Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,24 +1,29 @@
 using System;
 using System.Configuration.Assemblies;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
+        int number, byear;
+        string userName;
         DisplayWelcome();
-        PromptUsername();
-        PromptUserNumber();
-
+        userName = PromptUsername();
+        number = PromptUserNumber();
+        PromptUserBirthYear(out byear);
+        DisplayResult(userName, number, byear);
     }
 
     static void DisplayWelcome()
     {
         Console.WriteLine("Welcome to the program");
     }
-    static void PromptUsername()
+    static string PromptUsername()
     {
         Console.Write("Please enter your name: ");
         string name = Console.ReadLine();
+        return name;
     }
     static int PromptUserNumber()
     {
@@ -29,8 +34,16 @@ class Program
     }
     static void PromptUserBirthYear(out int birthYear)
     {
-        
+        Console.Write("Please enter the year you were born: ");
+        birthYear = int.Parse(Console.ReadLine());
+
+    }
+    static void DisplayResult(string userName, int userNumber, int byear)
+    {
+        int square = userNumber * userNumber, age = 2026 - byear;
+        Console.WriteLine($"Brother / Sister {userName}, the square of your number is {square}");
+        Console.WriteLine($"Brother / Sister {userName}, you will turn {age} this year.");
     }
     
-
+    
 }
