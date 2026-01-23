2956edffe9829ab04d024a3a2b368547751085e8
khata <hatakenaka@byui.edu>
2026-01-22 23:07:34 -0700

    Finished Learning 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Apple";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2022;
+        job1._endYear = 2025;
+
+
+        Job job2 = new Job();
+        job2._company = "Tesla";
+        job2._jobTitle = "Computer Engineer";
+        job2._startYear = 2019;
+        job2._endYear = 2022;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Kosei Hatakenaka";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        
+        resume1.DisplayResume();
     }
 }
+
