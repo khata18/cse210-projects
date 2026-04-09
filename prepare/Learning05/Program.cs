8cb795b86d234a1dcc853e8feffe2dc39099e9d7
khata <hatakenaka@byui.edu>
2026-04-08 23:16:18 -0600

    completed Learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,17 @@
-using System;
-
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes =
+        [
+            new Square("Red", 5),
+            new Rectangle("Blue", 4, 6),
+            new Circle("Green", 3),
+        ];
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
     }
 }
